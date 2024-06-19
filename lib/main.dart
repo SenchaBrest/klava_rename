74f105a79bb53801d9285d90bab2ec3477fcce37
sencha <senchabrest@gmail.com>
2024-06-19 16:50:25 +0300

    init sf2 settings

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'settings_manager.dart';
 import 'piano-lib/piano.dart';
-import 'package:flutter_midi/flutter_midi.dart';
+import 'screens/sf2_settings.dart';
+// import 'package:flutter_midi/flutter_midi.dart';
 
 void main() {
   runApp(KlavaRename());
 }
 
 class KlavaRename extends StatefulWidget {
+  const KlavaRename({super.key});
+
   @override
   _KlavaRenameState createState() => _KlavaRenameState();
 }
 
 class _KlavaRenameState extends State<KlavaRename> {
-  FlutterMidi flutterMidi = FlutterMidi();
+  // FlutterMidi flutterMidi = FlutterMidi();
   SettingsManager settingsManager = SettingsManager();
   Map<NotePosition, String> settings = {};
   bool isLoading = true;
   bool isBlocking = false;
+  bool showSettings = false;
   late NotePosition tappedNote;
   String pressedKey = "";
 
@@ -31,8 +35,8 @@ class _KlavaRenameState extends State<KlavaRename> {
   }
 
   void loadSoundFont() async {
-    ByteData byteData = await rootBundle.load('assets/sf2/Super_Nintendo_Unofficial_update.sf2');
-    flutterMidi.prepare(sf2: byteData, name: "Super_Nintendo_Unofficial_update.sf2");
+    // ByteData byteData = await rootBundle.load('assets/sf2/Super_Nintendo_Unofficial_update.sf2');
+    // flutterMidi.prepare(sf2: byteData, name: "Super_Nintendo_Unofficial_update.sf2");
   }
 
   void loadSettings() async {
@@ -62,6 +66,7 @@ class _KlavaRenameState extends State<KlavaRename> {
             });
           } else {
             playNote(settingsManager.getNoteForSetting(settings, pressedKey));
+            print(1);
           }
         }
       },
@@ -90,86 +95,140 @@ class _KlavaRenameState extends State<KlavaRename> {
                   },
                 ),
               ),
-              if (isBlocking)
-                Positioned.fill(
-                  child: Container(
-                    color: Colors.black87,
-                    child: Center(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          RichText(
-                            text: TextSpan(
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 32,
-                              ),
-                              children: [
-                                const TextSpan(text: "You set up "),
-                                TextSpan(
-                                  text: "${tappedNote.note.name}${tappedNote.octave}",
-                                  style: const TextStyle(
-                                    color: Colors.red,
-                                    fontSize: 32,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-
-                          const SizedBox(height: 20),
-                          const Text(
-                            "Press any key to continue",
-                            style: TextStyle(color: Colors.white, fontSize: 24),
-                          ),
-                          const SizedBox(height: 20),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SizedBox(
-                                width: 180,
-                                child: CupertinoButton(
-                                  color: Colors.blueGrey,
-                                  padding: EdgeInsets.zero,
-                                  child: const Icon(
-                                    Icons.cancel,
-                                    size: 30,
-                                    color: Colors.white,
-                                  ),
-                                  onPressed: () {
-                                    setState(() {
-                                      isBlocking = false;
-                                    });
-                                  },
-                                ),
-                              ),
-                              const SizedBox(width: 10),
-                              SizedBox(
-                                width: 60,
-                                child: CupertinoButton(
-                                  color: Colors.red,
-                                  padding: EdgeInsets.zero,
-                                  child: const Icon(
-                                    Icons.delete,
-                                    size: 30,
-                                    color: Colors.white,
-                                  ),
-                                  onPressed: () {
-                                    setState(() {
-                                      isBlocking = false;
-                                    });
-                                    settingsManager.setSettingForNote(settings, tappedNote, '');
-                                    saveSettings();
-                                  },
-                                ),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
+              if (isBlocking) ...[
+                AbsorbPointer(
+                  absorbing: true,
+                  child: Opacity(
+                    opacity: 0.6,
+                    child: Container(
+                      color: Colors.black,
                     ),
                   ),
                 ),
+                buildBlockingWidget(),
+              ],
+              Positioned(
+                top: 40,
+                right: 20,
+                child: isBlocking
+                    ? Container()
+                    : CupertinoButton(
+                  color: Colors.grey.withOpacity(0.5),
+                  padding: EdgeInsets.all(8),
+                  child: const Icon(
+                    Icons.settings,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      showSettings = true;
+                    });
+                  },
+                ),
+              ),
+              // Внутри метода build основного виджета
+              // Внутри метода build основного виджета
+              if (showSettings)
+                SettingsWidget(
+                  onSave: (int option1, int option2, int option3, int option4) {
+                    setState(() {
+                      showSettings = false;
+                      print('Selected Option 1: $option1');
+                      print('Selected Option 2: $option2');
+                      print('Selected Option 3: $option3');
+                    });
+                  },
+                  onCancel: () {
+                    setState(() {
+                      showSettings = false;
+                    });
+                  },
+                ),
+
+
+            ],
+
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildBlockingWidget() {
+    return Positioned.fill(
+      child: Container(
+        color: Colors.black87,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              RichText(
+                text: TextSpan(
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
+                  ),
+                  children: [
+                    const TextSpan(text: "You set up "),
+                    TextSpan(
+                      text: "${tappedNote.note.name}${tappedNote.octave}",
+                      style: const TextStyle(
+                        color: Colors.red,
+                        fontSize: 32,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                "Press any key to continue",
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 180,
+                    child: CupertinoButton(
+                      color: Colors.blueGrey,
+                      padding: EdgeInsets.zero,
+                      child: const Icon(
+                        Icons.cancel,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          isBlocking = false;
+                        });
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  SizedBox(
+                    width: 60,
+                    child: CupertinoButton(
+                      color: Colors.red,
+                      padding: EdgeInsets.zero,
+                      child: const Icon(
+                        Icons.delete,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          isBlocking = false;
+                        });
+                        settingsManager.setSettingForNote(settings, tappedNote, '');
+                        saveSettings();
+                      },
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
@@ -178,9 +237,9 @@ class _KlavaRenameState extends State<KlavaRename> {
   }
 
   void playNote(NotePosition? position) {
-    if (position != null) {
-      int midiNumber = position.octave * 12 + position.note.index + 21;
-      flutterMidi.playMidiNote(midi: midiNumber);
-    }
+    // if (position != null) {
+    //   int midiNumber = position.octave * 12 + position.note.index + 21;
+    //   flutterMidi.playMidiNote(midi: midiNumber);
+    // }
   }
 }
